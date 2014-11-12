838fdbb6b932f87fd76a471d43bd0209264993fe
Leticia Alves <leticia_borges.alves@hotmail.com>
2014-11-12 16:38:38 -0200

    Cadastro de Telas Completo

--- Administracao/AdministracaoModel/src/main/java/br/com/administracao/model/Modulo.java
+++ Administracao/AdministracaoModel/src/main/java/br/com/administracao/model/Modulo.java
@@ -3,31 +3,71 @@ package br.com.administracao.model;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Modulo implements Serializable{
-	
+public class Modulo implements Serializable {
+
 	private Long nro;
 	private String nome;
 	private Projeto projeto = new Projeto();
-	
+
 	public Long getNro() {
 		return nro;
 	}
+
 	public void setNro(Long nro) {
 		this.nro = nro;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Projeto getProjeto() {
 		return projeto;
 	}
+
 	public void setProjeto(Projeto projeto) {
 		this.projeto = projeto;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nro == null) ? 0 : nro.hashCode());
+		result = prime * result + ((projeto == null) ? 0 : projeto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Modulo other = (Modulo) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nro == null) {
+			if (other.nro != null)
+				return false;
+		} else if (!nro.equals(other.nro))
+			return false;
+		if (projeto == null) {
+			if (other.projeto != null)
+				return false;
+		} else if (!projeto.equals(other.projeto))
+			return false;
+		return true;
+	}
 
 }
--- Administracao/AdministracaoModel/src/main/java/br/com/administracao/model/Tela.java
+++ Administracao/AdministracaoModel/src/main/java/br/com/administracao/model/Tela.java
@@ -5,35 +5,79 @@ import java.util.List;
 
 @SuppressWarnings("serial")
 public class Tela implements Serializable {
-	
+
 	private Long nro;
 	private String nome;
 	private Modulo modulo = new Modulo();
-	private List<Funcoes> listaFuncoes; 
-	
+	private List<Funcoes> listaFuncoes;
+
 	public Long getNro() {
 		return nro;
 	}
+
 	public void setNro(Long nro) {
 		this.nro = nro;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Modulo getModulo() {
 		return modulo;
 	}
+
 	public void setModulo(Modulo modulo) {
 		this.modulo = modulo;
 	}
+
 	public List<Funcoes> getListaFuncoes() {
 		return listaFuncoes;
 	}
+
 	public void setListaFuncoes(List<Funcoes> listaFuncoes) {
 		this.listaFuncoes = listaFuncoes;
-	}		
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((modulo == null) ? 0 : modulo.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nro == null) ? 0 : nro.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tela other = (Tela) obj;
+		if (modulo == null) {
+			if (other.modulo != null)
+				return false;
+		} else if (!modulo.equals(other.modulo))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nro == null) {
+			if (other.nro != null)
+				return false;
+		} else if (!nro.equals(other.nro))
+			return false;
+		return true;
+	}
 
 }
